3de711627150cede12548e9bd6408d946e1f0b4b
Rodion <Keksonatorver2@gmail.com>
2025-11-02 09:51:20 +0300

    Версия   1.0

--- contacts.py
+++ contacts.py
@@ -1,38 +1,64 @@
+import json
+from json import JSONDecodeError
+
+CONTACT_JSON = 'contacts_json.json'
+
+
+def try_read_json(file_json):
+    try:
+        with open(file_json, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print("Файл не найден")
+        return {}
+    except  JSONDecodeError:
+        print("Невозможно прочитать файл")
+        return {}
+
+
+def save_json_data(data):
+    with open(CONTACT_JSON, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
 
 def show_contacts(contacts):
     if not contacts:
-        print("Контактов пока нет!")
-        return
+        print("Список контактов пуст")
+    else:
+        value = contacts['contacts']
+        print("--- Все контакты ---")
+        print(value)
 
-    print("--- Все контакты ---")
-    for contact_position, contact_data in enumerate(contacts, 1):
-        print(f"{contact_position}. {contact_data['name']}")
-        print(f"{' '*4}Телефон: {contact_data['phone']}")
-        print(f"{' '*4}Email: {contact_data['email']}")
-        print('-'*15)
 
-def add_contact(contacts: list):
-    print('--- Добавление нового контакта ---')
+def add_contact(contacts):
+    print("--- Добавление нового контакта ---")
     contact_name = input("Введите имя:")
     contact_phone = input("Введите телефон:")
     contact_email = input("Введите email:")
-    contacts.append(
-        {
-        'name': contact_name,
-        'phone': contact_phone,
-        'email':contact_email
-         }
-    )
-    print(f"Контакт {contact_name} успешно добавлен!")
-    return contacts
+    new_id = len(contacts['contacts']) + 1 if contacts else 1
+    if contacts:
+        contacts['contacts'].append(
+            {
+                'id': new_id,
+                'name': contact_name,
+                'phone': contact_phone,
+                'email': contact_email
+            }
+        )
+        return contacts
+    else:
+        contacts = {"contacts":
+            [{
+                'id': new_id,
+                'name': contact_name,
+                'phone': contact_phone,
+                'email': contact_email}]
+        }
+        return contacts
 
 
 def search_contacts(contacts):
-    if not contacts:
-        print("Контактов для поиска нет!")
-        return
-
     search_item = input('Введите имя, телефон или email для поиска:').strip().lower()
 
     if not search_item:
@@ -40,69 +66,80 @@ def search_contacts(contacts):
         return
 
     found_contacts = []
-    for contact_data in contacts:
-        contact_value = [contact_val.lower() for contact_val in contact_data.values()]
-        for i in contact_value:
-            if search_item in i:
-                found_contacts.append(contact_data)
-                break
+    for contact_data in contacts["contacts"]:
+        if search_item in contact_data["name"].strip().lower():
+            found_contacts.append(contact_data)
+            continue
+        if search_item in contact_data["phone"].strip().lower():
+            found_contacts.append(contact_data)
+            continue
+        if search_item in contact_data["email"].strip().lower():
+            found_contacts.append(contact_data)
+            continue
 
     if found_contacts:
         print(f"Найдено контактов:{len(found_contacts)}")
-        show_contacts(found_contacts)
+
+        print(found_contacts)
+        return found_contacts
 
     else:
         print('Контакты не найдены.')
 
-def del_contact(contacts: list):
+
+def del_contact(contacts: dict):
     if not contacts:
         print("Контактов для удаления нет!")
         return contacts
 
     show_contacts(contacts)
-    user_chose = input("Введите номер контакта на удаление:")
+    try:
+        user_chose = int(input("Введите id номер контакта на удаление: "))
+    except Exception as e:
+        print(f"{e}")
+        print("Введите номер контакта!")
+        return contacts
+    for index, data in enumerate(contacts['contacts']):
+        if user_chose == data['id']:
+            try:
+                contacts['contacts'].pop(index)
+            except Exception as e:
+                print(f"При удалении возникла ошибка: {e}")
+            break
+    print("Такого контакта нет")
+    return contacts
 
-    if int(user_chose) in range(1, len(contacts)+1):
-
-        user_arge = input("Подтвердите удаление контакта Д/Н : ").lower().strip()
-        if user_arge == 'y' or user_arge == 'д':
-            contacts.pop(int(user_chose)-1)
-            print("Контакт успешно удалён")
-            return contacts
-        else:
-            return
-    else:
-        print('Введите номер контакта!')
-        return
 
 def main():
     print('Добро пожаловать в контактную книгу!')
-    contacts = []
+    contacts = try_read_json(CONTACT_JSON)
 
     while True:
-        print('\n'+'='*30)
+        print('\n' + '=' * 30)
         print('1. Добавить контакт')
         print('2. Показать все контакты')
         print('3. Поиск контакта')
         print('4. Удалить контакт')
         print('5. Выйти')
-        print( '=' * 30 )
+        print('=' * 30)
 
         user_input = input('Выберите действие (1-5):')
 
         if user_input == '1':
-            add_contact(contacts)
+            contacts = add_contact(contacts)
         elif user_input == '2':
             show_contacts(contacts)
         elif user_input == '3':
             search_contacts(contacts)
         elif user_input == '4':
-            del_contact(contacts)
+            contacts = del_contact(contacts)
         elif user_input == '5':
+            save_json_data(contacts)
             print("До свидания!")
             break
         else:
             print('\n' + "Выберите один из пунктов меню")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
